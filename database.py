15866ad58ae985cc258956c0d441e53f63e48a10
escalacwb <escalao@gmail.com>
2025-08-12 10:41:33 -0400

    TESTE

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
-# --- FUNÇÕES PARA O APLICATIVO STREAMLIT ---
+# --- FUNÇÕES PARA O APLICATIVO STREAMLIT (NÃO MUDAM) ---
 
 def get_db_url():
     if hasattr(st, 'secrets') and st.secrets.get("DB_URL"):
@@ -18,23 +18,20 @@ def init_connection_pool():
     db_url = get_db_url()
     if not db_url:
         raise ValueError("URL do banco de dados não encontrada.")
-    # Ajuste o número máximo de conexões se necessário
-    return pool.SimpleConnectionPool(1, 30, dsn=db_url)
+    return pool.SimpleConnectionPool(1, 10, dsn=db_url)
 
 def get_connection():
     connection_pool = init_connection_pool()
     if connection_pool:
-        # st.toast("Obtendo uma conexão...")
         return connection_pool.getconn()
     return None
 
 def release_connection(conn):
     connection_pool = init_connection_pool()
     if connection_pool and conn:
-        # st.toast("Liberando a conexão...")
         connection_pool.putconn(conn)
 
-# --- FUNÇÃO PARA SCRIPTS INDEPENDENTES ---
+# --- NOVA FUNÇÃO PARA SCRIPTS INDEPENDENTES ---
 
 def get_script_connection():
     load_dotenv()
@@ -44,13 +41,7 @@ def get_script_connection():
         return None
     try:
         conn = psycopg2.connect(db_url)
-        print("Conexão direta estabelecida com sucesso.")
         return conn
     except Exception as e:
         print(f"Erro ao tentar conectar ao banco de dados: {e}")
         return None
-
-def close_script_connection(conn):
-    if conn:
-        conn.close()
-        print("Conexão direta fechada.")
